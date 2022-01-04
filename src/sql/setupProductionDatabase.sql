526a25e1b2940d58c1a90bdee70f7d33a70acfa6
Marie Stumbo <mariestumbo87@gmail.com>
2022-01-04 15:40:24 -0500

    Finish production database setup

--- src/sql/setupProductionDatabase.sql
+++ src/sql/setupProductionDatabase.sql
@@ -97,9 +97,61 @@ from mcuEdited;
 select * from Person;
 
 select distinct
-	e.`name`,
-    e.`role`,
+	p.`name`,
+    p.idPerson,
+    r.`role`,
     r.idRole
-from mcuEdited e
-inner join Role r on e.`role`=r.`role`
-order by e.`name`;
+from Person p
+inner join mcuEdited e on p.`name`=e.`name`
+inner join Role r on e.`role` = r.`role`
+order by p.`name`;
+
+insert into Person_Role (idPerson, idRole)
+select distinct
+	p.idPerson,
+    r.idRole
+from Person p
+inner join mcuEdited e on p.`name`=e.`name`
+inner join Role r on e.`role` = r.`role`;
+
+select * from Person_Role;
+
+select
+	p.`name`,
+    p.idPerson,
+    r.`role`,
+    r.idRole
+from Person p
+inner join Person_Role pr on p.idPerson=pr.idPerson
+inner join Role r on pr.idRole = r.idRole
+order by p.`name`;
+
+select
+	mv.title,
+    mv.idMovie,
+    p.`name`,
+    p.idPerson
+from Movie mv
+inner join mcuEdited e on mv.title = e.title
+inner join Person p on e.`name` = p.`name`;
+
+insert into Movie_Person (idMovie, idPerson)
+select
+	mv.idMovie,
+    p.idPerson
+from Movie mv
+inner join mcuEdited e on mv.title = e.title
+inner join Person p on e.`name` = p.`name`;
+
+select * from Movie_Person;
+
+select
+	mv.title,
+    mv.idMovie,
+    p.`name`,
+    p.idPerson
+from Movie mv
+inner join Movie_Person mp on mv.idMovie = mp.idMovie
+inner join Person p on mp.idPerson = p.idPerson;
+
+drop table mcuEdited;
